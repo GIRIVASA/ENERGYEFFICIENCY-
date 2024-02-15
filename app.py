8e96238159333a372c5e4c90cb9085588c14dcba
GIRI VASAN <88620239+GIRIVASA@users.noreply.github.com>
2024-02-15 11:39:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+import pickle
+import numpy as np
+import pandas as pd
+from flask import Flask, url_for, render_template, request
+
+app = Flask(__name__)
+
+heating_metrics = pd.read_csv('heating_metrics.csv', index_col='Unnamed: 0')
+cooling_metrics = pd.read_csv('cooling_metrics.csv', index_col='Unnamed: 0')
+
+heating_load_model_name = heating_metrics['Accuracy'].idxmax()
+cooling_load_model_name = cooling_metrics['Accuracy'].idxmax()
+
+cooling_load_model = pickle.load(open(f'Artifact/saved models/{cooling_load_model_name}/cooling_load_model.pkl', 'rb'))
+heating_load_model = pickle.load(open(f'Artifact/saved models/{heating_load_model_name}/heating_load_model.pkl', 'rb'))
+transformer = pickle.load(open(f'Artifact/transformer/scaler.pkl', 'rb'))
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+
+	if request.method == 'POST':
+		
+		data_form = request.form
+		data = [data_form[i] for i in data_form]
+		data = np.array(data[:-1])
+		data = data.reshape(1, -1)
+		data = transformer.transform(data)
+		print(data)
+
+		cooling_button = request.form.get('cooling_button')
+		if cooling_button is not None:
+			# cooling_load_model = pickle.load(open(f'saved models/{cooling_load_model_name}/cooling_load_model.pkl', 'rb'))
+			prediction = cooling_load_model.predict(data)
+			button = 'cooling'
+			print('****************************************')
+			
+		else:
+			# heating_load_model = pickle.load(open(f'saved models/{heating_load_model_name}/heating_load_model.pkl', 'rb'))
+			prediction = heating_load_model.predict(data)
+
+			button = None
+
+		return render_template('home.html', prediction=prediction[0], button=button)
+
+	else:
+		return render_template('home.html')
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import pymongo
+from energy_efficiency.components.data_ingestion import DataIngestion
+from energy_efficiency.components.model_training import ModelTrainer
+from energy_efficiency.components.data_transformation import DataTransformation
+from energy_efficiency.exception import EnergyEfficiencyException
+
+database_name = "Ineuron_project"
+collection_name = "Energy_Efficiency"
+mongo_client = pymongo.MongoClient("mongodb+srv://jebathurai:<password>@cluster0.irlqnsu.mongodb.net/?retryWrites=true&w=majority")
+
+
+if __name__ == '__main__':
+
+	try:
+		data_ingestion = DataIngestion(mongo_client=mongo_client, database_name=database_name, 
+									   collection_name=collection_name)
+
+		train_data, test_data = data_ingestion.initiate_data_ingestion()
+
+		data_transformation = DataTransformation(train_data=train_data, test_data=test_data)
+		scaled_train_data, scaled_test_data = data_transformation.initiate_data_transformation()
+
+		model_training = ModelTrainer(train_data=scaled_train_data, test_data=scaled_test_data)
+		model_training.initiate_model_trainer()
+
+	except Exception as e:
+		raise EnergyEfficiencyException(e, sys)
+
